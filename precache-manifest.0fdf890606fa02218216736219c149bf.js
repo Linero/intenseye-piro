95f17006ad11de28c4957137b576b3a28cfd24ca
Mateusz Kotasiński <mateusz.kotasinski@gmail.com>
2019-06-30 17:13:08 +0200

    Updates

--- precache-manifest.0fdf890606fa02218216736219c149bf.js
+++ precache-manifest.0fdf890606fa02218216736219c149bf.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/intenseye-piro/static/js/runtime~main.b2156389.js"
   },
   {
-    "revision": "79a5dc601a5a32105392",
-    "url": "/intenseye-piro/static/js/main.79a5dc60.chunk.js"
+    "revision": "7a8e8afbd686dc12833b",
+    "url": "/intenseye-piro/static/js/main.7a8e8afb.chunk.js"
   },
   {
     "revision": "6b8f2dd792c6c518f1eb",
     "url": "/intenseye-piro/static/js/2.6b8f2dd7.chunk.js"
   },
   {
-    "revision": "79a5dc601a5a32105392",
+    "revision": "7a8e8afbd686dc12833b",
     "url": "/intenseye-piro/static/css/main.01e33647.chunk.css"
   },
   {
-    "revision": "371296160b15696600d7c8717a9408ff",
+    "revision": "78576db460a661a86acfe37939383d8e",
     "url": "/intenseye-piro/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/intenseye-piro/precache-manifest.8245c124f06ef08cb5e9833381f8ee74.js"
+  "/intenseye-piro/precache-manifest.0fdf890606fa02218216736219c149bf.js"
 );
 
 workbox.clientsClaim();
